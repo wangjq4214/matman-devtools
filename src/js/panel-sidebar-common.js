b9045af4d861f030a431a5f62cd4e40e90603d9f
helinjiang <me@helinjiang.com>
2018-12-02 23:57:20 +0800

    refactor: 本地调试

--- src/js/panel-sidebar-common.js
+++ src/js/panel-sidebar-common.js
@@ -1,3 +1,9 @@
+const gMockData = {
+    data1: {
+        hellow: 'world'
+    }
+};
+
 const gUtils = (function () {
     /**
      * 发送消息给 content script，并处理回调
@@ -6,6 +12,15 @@ const gUtils = (function () {
      * @param {Function} callback
      */
     function sendMsgToContentScript(message, callback) {
+        // 这里主要是为了调试之用
+        if ((typeof chrome === 'undefined') || !chrome.tabs || !chrome.tabs.sendMessage) {
+            callback({
+                type: 'SEND_RESPONSE_GET_TIPS',
+                data: gMockData.data1
+            });
+            return;
+        }
+
         // https://developer.chrome.com/extensions/devtools#content-script-to-devtools
         // https://developer.chrome.com/extensions/tabs#method-sendMessage
         chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, message, function (response) {
@@ -21,6 +36,15 @@ const gUtils = (function () {
      * @param {Function} callback
      */
     function listenMsgFromContentScript(callback) {
+        // 这里主要是为了调试之用
+        if ((typeof chrome === 'undefined') || !chrome.runtime || !chrome.runtime.onMessage) {
+            callback({
+                type: 'CONTENT_SCRIPT_SEND_MESSAGE_AFTER_SELECTED_ELEMENT',
+                data: gMockData.data1
+            });
+            return;
+        }
+
         // https://developer.chrome.com/extensions/runtime#event-onMessage
         chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
             console.log('[panel-sidbar.js][listenMsgFromContentScript] receive message', message);
