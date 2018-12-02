432a3c5559950071768838050f1f9b9be04ffd72
helinjiang <onehlj@qq.com>
2018-12-02 21:25:14 +0800

    refactor: 优化代码结构

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -1,25 +1,20 @@
-console.log('--hello--content-script.js', window.jQuery);
+console.log('[matman-devtools] content scripts loaded');
 
 // 加载完 jQuery 之后，可以做一些其他的事情
 $(document).ready(function () {
-    var size = window.jQuery('div').length;
-    console.log('--hello--content-script.js 222222', size);
-
-    // setTimeout(() => {
-    //     chrome.runtime.sendMessage({ msg: '试试主动从 content-script.js 能否发送信息' });
-    // }, 5000);
+    console.log('[matman-devtools] jQuery is ready!');
 });
 
 // 监听来自 DevTools page 的消息，然后再回调信息
 // 例如可获取到 DOM 或 window 等信息，再传回到 DevTools page 做展示
 // DevTools page 通过 chrome.tabs.sendMessage 来发送消息
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log('[content-script.js] chrome.runtime.onMessage', message);
+    console.log('[matman-devtools] receive message', message);
     // 接收到新的指令，获取到 DOM 或 window 的信息
 
     // 传回到 DevTools page
     sendResponse({
-        type: '[content-script.js] chrome.runtime.onMessage response',
+        type: 'CONTENT_SCRIPT_RSP',
         data: 'I got it, and reply to you: ' + (message && (typeof message === 'object') ? JSON.stringify(message) : message)
     });
 });
@@ -29,7 +24,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
  * @param selectedDom 当前选中的 DOM 元素
  */
 function setSelectedElement(selectedDom) {
-    console.log('[content-script.js] setSelectedElement', selectedDom);
+    console.log('[matman-devtools] selected dom', selectedDom);
 
     // 获取相关数据
     const data = {
@@ -42,7 +37,7 @@ function setSelectedElement(selectedDom) {
 
     // 传递数据到 DevTools page
     chrome.runtime.sendMessage({
-        type: '[content-script.js] sendMessage',
+        type: 'CONTENT_SCRIPT_SEND_MESSAGE_AFTER_SELECTED_ELEMENT',
         data: data
     });
 }
@@ -51,25 +46,25 @@ function setSelectedElement(selectedDom) {
 // 使用场景：Injected Scripts 调用 window.postMessage({}, '*'); 然后在这里接收到数据
 // Messaging from Injected Scripts to the DevTools Page
 // https://developer.chrome.com/extensions/devtools#evaluated-scripts-to-devtools
-window.addEventListener('message', function (event) {
-    console.log('------window.addEventListener message------', event, event.data);
-    // Only accept messages from the same frame
-    if (event.source !== window) {
-        return;
-    }
-
-    var message = event.data;
-
-    // Only accept messages that we know are ours
-    if (typeof message !== 'object' || message === null ||
-        !message.source === 'my-devtools-extension') {
-        return;
-    }
-
-    chrome.runtime.sendMessage({
-        type: 'content-script.js sendMessage',
-        message: message
-    });
-});
+// window.addEventListener('message', function (event) {
+//     console.log('------window.addEventListener message------', event, event.data);
+//     // Only accept messages from the same frame
+//     if (event.source !== window) {
+//         return;
+//     }
+//
+//     var message = event.data;
+//
+//     // Only accept messages that we know are ours
+//     if (typeof message !== 'object' || message === null ||
+//         !message.source === 'my-devtools-extension') {
+//         return;
+//     }
+//
+//     chrome.runtime.sendMessage({
+//         type: 'content-script.js sendMessage',
+//         message: message
+//     });
+// });
 
 
--- src/js/panel-sidebar.js
+++ src/js/panel-sidebar.js
@@ -1,23 +1,23 @@
-// Create a connection to the background page
-var backgroundPageConnection = chrome.runtime.connect({
-    name: 'panel'
-});
-
-const inspectedWindowId = chrome.devtools.inspectedWindow.tabId;
-
-// backgroundPageConnection.postMessage({
-//     name: 'init',
-//     tabId: inspectedWindowId
+// // Create a connection to the background page
+// var backgroundPageConnection = chrome.runtime.connect({
+//     name: 'panel'
 // });
-
-// Listen to messages from the background page
-backgroundPageConnection.onMessage.addListener(function (message) {
-    console.log('[panel-sidbar.js] ------backgroundPageConnection.onMessage-----', message);
-
-    if (message.updatedTabId === inspectedWindowId) {
-        console.log('[panel-sidbar.js] ------curent inspected window-----');
-    }
-});
+//
+// const inspectedWindowId = chrome.devtools.inspectedWindow.tabId;
+//
+// // backgroundPageConnection.postMessage({
+// //     name: 'init',
+// //     tabId: inspectedWindowId
+// // });
+//
+// // Listen to messages from the background page
+// backgroundPageConnection.onMessage.addListener(function (message) {
+//     console.log('[panel-sidbar.js] ------backgroundPageConnection.onMessage-----', message);
+//
+//     if (message.updatedTabId === inspectedWindowId) {
+//         console.log('[panel-sidbar.js] ------curent inspected window-----');
+//     }
+// });
 
 var app = new Vue({
     el: '#app',
@@ -28,30 +28,20 @@ var app = new Vue({
     },
     methods: {
         handleClick: function (event) {
-            console.log('[panel-sidbar.js] --handleClick--');
-            this.tips = 'changed tips';
+            this.tips = 'loading...';
 
+            // 发送消息给 content script，并处理回调
             sendMsgToContentScript({
                 type: 'GET_NEW_TIPS'
             }, (response) => {
-                console.log('[panel-sidbar.js] --chrome.tabs.sendMessage from content scripts--', response);
                 this.tips = response;
             });
         }
     },
     created() {
-        console.log('[panel-sidbar.js] --created--');
-        const self = this;
-
-        // https://developer.chrome.com/extensions/runtime#event-onMessage
-        chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-            console.log('[panel-sidbar.js] --chrome.runtime.onMessage--', message);
-
-            sendResponse({
-                farewell: 'i got your message, and say ' + message && message.result && message.result.text
-            });
-
-            self.showData = JSON.stringify(message);
+        // 监听来自 content script 的消息，并处理回调
+        listenMsgFromContentScript((message) => {
+            this.showData = JSON.stringify(message);
         });
     }
 });
@@ -65,5 +55,23 @@ var app = new Vue({
 function sendMsgToContentScript(message, callback) {
     // https://developer.chrome.com/extensions/devtools#content-script-to-devtools
     // https://developer.chrome.com/extensions/tabs#method-sendMessage
-    chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, message, callback);
+    chrome.tabs.sendMessage(chrome.devtools.inspectedWindow.tabId, message, function (response) {
+        console.log('[panel-sidbar.js][sendMsgToContentScript] receive response', response);
+
+        callback(response);
+    });
+}
+
+/**
+ * 监听来自 content script 的消息，并处理回调
+ *
+ * @param {Function} callback
+ */
+function listenMsgFromContentScript(callback) {
+    // https://developer.chrome.com/extensions/runtime#event-onMessage
+    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+        console.log('[panel-sidbar.js][listenMsgFromContentScript] receive message', message);
+
+        callback(message);
+    });
 }
