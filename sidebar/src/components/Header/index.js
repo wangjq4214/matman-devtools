15a3315e1e2dc66c49cd607da490a0eb73d9f7a3
derekjqwang <derekjqwang@tencent.com>
2020-07-31 16:51:58 +0800

    fix: 清除执行功能

--- sidebar/src/components/Header/index.js
+++ sidebar/src/components/Header/index.js
@@ -1,23 +1,20 @@
 /* global chrome */
 import React from 'react';
-import { Layout, Typography, Button } from 'antd';
-import useCodeModel from '../../models/code';
+import { Layout, Typography } from 'antd';
 
 import styles from './index.module.scss';
 
 const Index = () => {
-  const { code } = useCodeModel();
-
-  const exec = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length) {
-        chrome.tabs.executeScript(tabs[0].id, {
-          code: `var useJquery=function(t){"use strict";function e(t,e){return $(t,e).length}function r(t,e){var r=$(t,e)[0];if(!r)return{isExist:!1};var n=document.defaultView.getComputedStyle(r);return{isExist:!0,width:n.width,height:n.height,lineHeight:n.lineHeight,isOneLine:parseInt(n.height)===parseInt(n.lineHeight),computedStyle:n}}return t.getAttr=function(t,e,r){var n=$(e,r);return $.trim(n.attr(t))},t.getBackgroundImageUrl=function(t,e){var n=r(t,e).computedStyle;return n&&(n.backgroundImage.match(/url\\("(.*)"\\)/)||[])[1]||""},t.getImageDomUrl=function(t,e){var r=$(t,e);return $.trim(r.attr("src"))},t.getStyle=r,t.getText=function(t,e){var r=$(t,e);return $.trim(r.text())},t.getTotal=e,t.isExist=function(t,r){return e(t,r)>0},t}({});
-          console.log((${code})())`,
-        });
-      }
-    });
-  };
+  // const exec = () => {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //     if (tabs.length) {
+  //       chrome.tabs.executeScript(tabs[0].id, {
+  //         code: `var useJquery=function(t){"use strict";function e(t,e){return $(t,e).length}function r(t,e){var r=$(t,e)[0];if(!r)return{isExist:!1};var n=document.defaultView.getComputedStyle(r);return{isExist:!0,width:n.width,height:n.height,lineHeight:n.lineHeight,isOneLine:parseInt(n.height)===parseInt(n.lineHeight),computedStyle:n}}return t.getAttr=function(t,e,r){var n=$(e,r);return $.trim(n.attr(t))},t.getBackgroundImageUrl=function(t,e){var n=r(t,e).computedStyle;return n&&(n.backgroundImage.match(/url\\("(.*)"\\)/)||[])[1]||""},t.getImageDomUrl=function(t,e){var r=$(t,e);return $.trim(r.attr("src"))},t.getStyle=r,t.getText=function(t,e){var r=$(t,e);return $.trim(r.text())},t.getTotal=e,t.isExist=function(t,r){return e(t,r)>0},t}({});
+  //         console.log((${code})())`,
+  //       });
+  //     }
+  //   });
+  // };
 
   return (
     <Layout.Header
@@ -30,9 +27,9 @@ const Index = () => {
       }}
     >
       <Typography.Title className={styles.title}>Matman</Typography.Title>
-      <Button type="primary" onClick={exec}>
+      {/* <Button type="primary" onClick={exec}>
         执行
-      </Button>
+      </Button> */}
     </Layout.Header>
   );
 };
