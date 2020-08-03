e75e05d472c839d57c517855e8d08b9106c99905
derekjqwang <derekjqwang@tencent.com>
2020-08-03 10:56:36 +0800

    feat: 添加代码保存功能

--- panel/src/models/code.js
+++ panel/src/models/code.js
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+/* global chrome */
+import { useState, useEffect } from 'react';
 import { createModel } from 'hox';
 
 function useCode() {
   // 元素跟踪
   const [code, setCode] = useState('// Typing....');
+
+  useEffect(() => {
+    chrome.storage.local.get({ code: '// Typing....' }, (item) => {
+      setCode(item.code);
+    });
+  }, []);
+
+  useEffect(() => {
+    chrome.storage.local.set({ code: code });
+  }, [code]);
 
   return {
     code,
